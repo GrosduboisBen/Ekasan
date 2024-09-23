05b36dade9fdd4ee4f563af001551baed326f8be
benoit.gros-dubois <benoit.gros-dubois@polynom.io>
2024-09-23 17:36:53 +0200

    RM mobile

--- front-mobile-gpe/config.js
+++
@@ -1,5 +0,0 @@
-const IP_ADDRESS = '0.0.0.0';
-const API_PORT = '8090';
-const IA_API_PORT = '8000';
-export const API_URL = 'http://' + IP_ADDRESS + ':' + API_PORT
-export const IA_API_URL = 'http://' + IP_ADDRESS + ':' + IA_API_PORT
